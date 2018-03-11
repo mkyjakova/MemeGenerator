c7b51025eeb91a074064833b0a2d93b74615ecf7
Unknown <martina.kyjakova@gmail.com>
2018-03-11 22:49:22 +0100

    Functionality resolved
    Randomness works!

--- src/main/java/cz/czechitas/webapp/controller/MemeGeneratorController.java
+++ src/main/java/cz/czechitas/webapp/controller/MemeGeneratorController.java
@@ -9,16 +9,35 @@ import org.springframework.web.servlet.*;
 @Controller
 public class MemeGeneratorController{
 
+    private static final List<String> vyroky = Arrays.asList(
+            "Be a pineapple: stand tall, wear a crown, and be sweet on the inside",
+            "My fake plants died because I did not pretend to water them",
+            "I am not a vegetarian because I love animals. I'm a vegetarian because I hate plants.",
+            "Plant lady is the new cat lady"
+            
+    );
+
     @RequestMapping("/meme.html")
     public ModelAndView zobrazMeme(){
-
         ModelAndView drzakNaDataJmenoStranky;
         drzakNaDataJmenoStranky = new ModelAndView("index");
+        drzakNaDataJmenoStranky.addObject("obrazek",1);
 
-        Random generatorNahodnychCisel = new Random();
+        Random generatorNahodnychCiselKytky = new Random();
+        int nahodneCisloKytky = generatorNahodnychCiselKytky.nextInt(5)+1;
+        drzakNaDataJmenoStranky.addObject("obrazek", nahodneCisloKytky);
 
-        int nahodneCislo = generatorNahodnychCisel.nextInt(6)+1;
-        drzakNaDataJmenoStranky.addObject("cislo", nahodneCislo);
+
+
+        Random generatorNahodnychCiselVyroky = new Random();
+        int nahodneCisloVyroku = generatorNahodnychCiselVyroky.nextInt(vyroky.size());
+
+        String vyrok;
+        vyrok= new String();
+        vyrok = vyroky.get(nahodneCisloVyroku);
+
+        drzakNaDataJmenoStranky.addObject("vyrok", vyrok);
+
 
         return  drzakNaDataJmenoStranky;
     }
