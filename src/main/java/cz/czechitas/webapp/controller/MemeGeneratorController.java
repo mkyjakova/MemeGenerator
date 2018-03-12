7b9f555b8049c3e86b69e4c93807387477db87f7
Unknown <martina.kyjakova@gmail.com>
2018-03-12 16:06:34 +0100

    Functionality working 50%
    Has occasional bugs to be fixed.

--- src/main/java/cz/czechitas/webapp/controller/MemeGeneratorController.java
+++ src/main/java/cz/czechitas/webapp/controller/MemeGeneratorController.java
@@ -15,30 +15,52 @@ public class MemeGeneratorController{
             "I am not a vegetarian because I love animals. I'm a vegetarian because I hate plants.",
             "Plant lady is the new cat lady.",
             "Maybe actually plants are farming us, giving us oxygen until we eventually decompose and they can consume us.",
-            "It's amazing how potatoes give us chips, fries and vodka. Get your shit together, every other vegetable."
+            "It's amazing how potatoes give us chips, fries and vodka. Get your shit together, every other vegetable.",
+            "If money is made of paper doesn't it mean money grows on trees?",
+            "Friends are like trees. They fall down if you hit them multiple times with an axe."
             
+    );
+    private static final List<Integer> cisla= Arrays.asList(
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8
     );
 
     @RequestMapping("/meme.html")
     public ModelAndView zobrazMeme(){
         ModelAndView drzakNaDataJmenoStranky;
+        Collections.shuffle(cisla);
+        
         drzakNaDataJmenoStranky = new ModelAndView("index");
         drzakNaDataJmenoStranky.addObject("obrazek",1);
 
-        Random generatorNahodnychCiselKytky = new Random();
-        int nahodneCisloKytky = generatorNahodnychCiselKytky.nextInt(8)+1;
-        drzakNaDataJmenoStranky.addObject("obrazek", nahodneCisloKytky);
+        int nahodneCisloKytky1 = cisla.get(0);
+        drzakNaDataJmenoStranky.addObject("obrazek1", nahodneCisloKytky1);
+
+        int nahodneCisloKytky2 = cisla.get(1);
+        drzakNaDataJmenoStranky.addObject("obrazek2", nahodneCisloKytky2);
 
 
 
-        Random generatorNahodnychCiselVyroky = new Random();
-        int nahodneCisloVyroku = generatorNahodnychCiselVyroky.nextInt(vyroky.size());
+        int nahodneCisloVyroku1 = cisla.get(2);
+        int nahodneCisloVyroku2 = cisla.get(3);
 
-        String vyrok;
-        vyrok= new String();
-        vyrok = vyroky.get(nahodneCisloVyroku);
+        String vyrok1;
+        vyrok1= new String();
+        vyrok1 = vyroky.get(nahodneCisloVyroku1);
 
-        drzakNaDataJmenoStranky.addObject("vyrok", vyrok);
+        drzakNaDataJmenoStranky.addObject("vyrok1", vyrok1);
+
+        String vyrok2;
+        vyrok2= new String();
+        vyrok2 = vyroky.get(nahodneCisloVyroku2);
+
+        drzakNaDataJmenoStranky.addObject("vyrok2", vyrok2);
 
 
         return  drzakNaDataJmenoStranky;
